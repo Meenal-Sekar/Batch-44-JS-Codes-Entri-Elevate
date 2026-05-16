da62a1b9c34b6f10c0ff1c5ac450baa45a4f8b7c
meenal-sekar <pandimeenal.entri.me>
2026-05-16 21:40:15 +0530

    code added

---
+++ React/session-50/tensor/src/App.jsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useRef, useState } from 'react'
+
+import * as faceapi from "face-api.js";
+
+function App() 
+{
+
+  const[emotion,setEmotion]=useState('Loading....')
+
+ const videoRef = useRef(); // direct html elements access, it update the
+  // visuals each and every second.
+
+
+  // load the models from model / video on
+  useEffect(()=>
+  {
+
+    async function start()
+    {
+       const MODEL_URL = "/models";
+
+        // Load models
+      await faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL);
+      await faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL);
+
+      console.log('Models added')
+
+      //start camera 
+         const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+
+       console.log("Camera Stream:", stream);
+
+        videoRef.current.srcObject = stream;
+      
+      setEmotion("Detecting...");
+
+
+    }
+
+    start();
+  },[])
+
+    //2.useEffect for detecting emotions
+     useEffect(() => {
+
+    const interval = setInterval(async () => {
+
+      if(!videoRef.current) return;
+
+      const detections = await faceapi.detectAllFaces(videoRef.current, 
+        new faceapi.TinyFaceDetectorOptions()).withFaceExpressions();
+
+        console.log(detections);
+
+        // [
+        // expressions: {
+        //   happy: 1,
+        //   sad: 5,
+        //   neutral: 10,
+        // }
+        // ]
+
+        if(detections.length > 0){
+          const expressions = detections[0].expressions;
+            
+          // [1, 2, 3].reduce(a, b => a + b);
+
+            //[1,2,3,4,5]
+            
+          const maxEmotion = Object.keys(expressions).
+          reduce((a, b) => expressions[a] > expressions[b] ? a : b);
+
+          console.log(maxEmotion);
+
+          setEmotion(maxEmotion);
+        }
+
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+
+
+return(
+<div  style={{ textAlign: "center", marginTop: "20px"}}>
+     <h1 style={{color:"blue"}}>Emotion Detection App</h1>
+      <video ref={videoRef}   autoPlay muted width="600" height="450" 
+      style={{border:"2px solid green"}} />
+
+      <h2>Emotion:{emotion}</h2>
+
+</div>
+)
+
+
+
+
+}
+
+export default App
